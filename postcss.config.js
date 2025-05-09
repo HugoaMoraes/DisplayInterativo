6db20335253e2723ed0ec217ac2d16a64f6066d2
Hugo Moraes <102623594+HugoaMoraes@users.noreply.github.com>
2025-05-09 12:38:18 -0300

    Fix PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,15 @@ export default {
   plugins: {
     'tailwindcss': {},
     'autoprefixer': {},
-    '@fullhuman/postcss-purgecss': {
-      content: [
-        './index.html',
-        './src/**/*.{js,jsx,ts,tsx}',
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: ['html', 'body']
+    '@fullhuman/postcss-purgecss'() {
+      return {
+        content: [
+          './index.html',
+          './src/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body']
+      }
     }
   },
 };
